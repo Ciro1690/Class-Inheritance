8bbcf0020686aefd2d711a87312d4374400b046f
Ciro <Ciro16@gmail.com>
2020-06-04 16:15:29 -0600

    updated bonuses

--- Business.rb
+++ Business.rb
@@ -1,22 +1,32 @@
 class Employee
 
-    attr_reader :salary
+    attr_reader :name, :salary, :title
+    attr_accessor :boss
 
-    def initialize(name, salary, title, boss)
+    def initialize(name, salary, title, boss = nil)
         @name = name
         @salary = salary
         @title = title
-        @boss = boss
+        self.boss = boss
     end
 
     def bonus(multiplier)
         bonus = @salary * multiplier
     end
+
 end
 
 class Manager < Employee
-    def initialize
+
+    attr_reader :employee
+
+    def initialize(name,salary,title,boss=nil)
+        super(name,salary,title,boss)
         @employees_under = []
+    end
+
+    def employees_under(employee)
+        @employees_under << employee
     end
 
     def bonus(multiplier)
@@ -25,5 +35,21 @@ class Manager < Employee
             total_salary += employee.salary
         end
         bonus = total_salary * multiplier
+        return bonus
     end
 end
+
+shawna = Employee.new("Shawna", 12000, "TA", "Darren")
+david = Employee.new("David", 10000, "TA", "Darren")
+darren = Manager.new("Darren", 78000, "TA Manager")
+ned = Manager.new("Ned", 1000000, "Founder", nil)
+
+ned.employees_under(darren)
+ned.employees_under(david)
+ned.employees_under(shawna)
+darren.employees_under(david)
+darren.employees_under(shawna)
+
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) # => 88_000
+p david.bonus(3) # => 30_000
